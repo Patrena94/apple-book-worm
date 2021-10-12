2db9155312f36e7ce22fdb9916a603596fa28ebe
Vashti <patrena@sbcglobal.net>
2021-10-11 19:38:39 -0700

    updated typeDef's

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,39 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+type User{
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+type Book{
+  // bookId: string
+  authors:[String]
+  description: String
+  title: String
+  // image: research
+  // link: research
+}
+type Auth{
+  token: ID!
+  user: User
+}
+}
   type Query {
-    helloWorld: String
+    me: User
+    users: [User]
+    user(username: String!): User
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    // saveBook(book: array, bookID: ID!, image: img!, link, link!): User
+    removeBook(bookId: ID!): User
+  }
+  
 `;
 
 // export the typeDefs
